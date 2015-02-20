c27a1d025b2940e39da4a28155e66e58b9623f4e
Alex Jolly <jolly_alex@yahoo.com>
2015-02-19 22:35:49 -0500

    Gets song title from YouTube

--- src/getText.js
+++ src/getText.js
@@ -1,5 +1,11 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 	if(message == "getText") {
-		sendResponse(document.body.innerHTML);
+		var url = document.URL;
+		// YouTube case
+		if (url.length >= 23 && document.URL.substring(0, 23) == "https://www.youtube.com") {
+			sendResponse(document.getElementById("eow-title").innerHTML);
+		} else {
+			alert("This website is not supported");
+		}
 	}
 });
