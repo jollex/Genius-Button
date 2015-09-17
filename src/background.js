63842cf9fcd44b3d7115e53cc2b6405bc6dd1dec
Alex Jolly <alex@alexjolly.me>
2015-09-16 21:05:18 -0400

    Added function for getting HTML response from Genius

--- src/background.js
+++ src/background.js
@@ -1,13 +1,40 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.executeScript(tab.id, {file:"src/getText.js"}, function() {
 		chrome.tabs.sendMessage(tab.id, "getText", function(response) {
-			var song = getSong(response);
-			//var genius_url = getGeniusURL(song);
+			var searchResponse = getSearchResponse(response);
+			var genius_url = getGeniusURL(searchResponse);
 			//chrome.tabs.create({url:genius_url});
 		});
 	});
 });
 
-function getSong(response) {
+function getSearchResponse(songTitle) {
+	// Base search URL
+	var baseURL = "http://genius.com/search?q=";
+
+	// Get the search URL
+	var words = songTitle.split(" ");
+	var requestURL = baseURL + words.join("+");
+
+	var HttpClient = function() {
+    	this.get = function(aUrl, aCallback) {
+        	var anHttpRequest = new XMLHttpRequest();
+        	anHttpRequest.onreadystatechange = function() { 
+            	if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                	aCallback(anHttpRequest.responseText);
+        	}
+
+        	anHttpRequest.open("GET", aUrl, true);            
+        	anHttpRequest.send(null);
+    	}
+	}
+
+	aClient = new HttpClient();
+	aClient.get(requestURL, function(response) {
+		return getGeniusURL(response);
+	});
+}
+
+function getGeniusURL(response) {
 	console.log(response);
 }
--- src/getText.js
+++ src/getText.js
@@ -2,10 +2,24 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 	if(message == "getText") {
 		var url = document.URL;
 		// YouTube case
-		if (url.length >= 23 && document.URL.substring(0, 23) == "https://www.youtube.com") {
+		if (url.length >= 23 && url.substring(0, 23) == "https://www.youtube.com") {
+			// TODO: Make sure they're watching a video.
 			sendResponse(document.getElementById("eow-title").innerHTML);
+		} else if (url.length >= 22 && url.substring(0, 22) == "https://soundcloud.com") {
+			var classResultArray = document.getElementsByClassName("playbackSoundBadge__title sc-truncate");
+			if (classResultArray.length > 0) {
+				sendResponse(classResultArray[0].title);
+			} else {
+				var response = prompt("[Genius Button] A song isn't playing", "Please enter song and artist here");
+				if (response != null) {
+					sendResponse(response);
+				}
+			}
 		} else {
-			alert("This website is not supported");
+			var response = prompt("[Genius Button] This website is not supported", "Please enter song and artist here");
+			if (response != null) {
+    			sendResponse(response);
+			}
 		}
 	}
 });
