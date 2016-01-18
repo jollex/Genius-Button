a302cbf7036fef3bd229a5b1947e851db14fe044
Alex Jolly <alex@alexjolly.me>
2016-01-18 03:36:30 -0500

    Added jQuery, finished implementation, consolidated everything into one function, cleaned up.

--- src/background.js
+++ src/background.js
@@ -1,40 +1,25 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.executeScript(tab.id, {file:"src/getText.js"}, function() {
 		chrome.tabs.sendMessage(tab.id, "getText", function(response) {
-			var searchResponse = getSearchResponse(response);
-			var genius_url = getGeniusURL(searchResponse);
-			//chrome.tabs.create({url:genius_url});
+            getGeniusURL(response, function(genius_url) {
+                chrome.tabs.create({url:genius_url});
+            });
 		});
 	});
 });
 
-function getSearchResponse(songTitle) {
-	// Base search URL
+function getGeniusURL(songTitle, cb) {
 	var baseURL = "http://genius.com/search?q=";
+	var requestURL = baseURL + encodeURI(songTitle.trim().split(" ").join("+"));
 
-	// Get the search URL
-	var words = songTitle.split(" ");
-	var requestURL = baseURL + words.join("+");
-
-	var HttpClient = function() {
-    	this.get = function(aUrl, aCallback) {
-        	var anHttpRequest = new XMLHttpRequest();
-        	anHttpRequest.onreadystatechange = function() { 
-            	if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                	aCallback(anHttpRequest.responseText);
-        	}
-
-        	anHttpRequest.open("GET", aUrl, true);            
-        	anHttpRequest.send(null);
-    	}
-	}
-
-	aClient = new HttpClient();
-	aClient.get(requestURL, function(response) {
-		return getGeniusURL(response);
-	});
-}
-
-function getGeniusURL(response) {
-	console.log(response);
+    var anHttpRequest = new XMLHttpRequest();
+    anHttpRequest.onreadystatechange = function() {
+        if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200) {
+            var html = anHttpRequest.responseText;
+            var url = $(html).find('.search_result').first().find('a').attr('href');
+            cb(url);
+        }
+    }
+    anHttpRequest.open("GET", requestURL, true);
+    anHttpRequest.send(null);
 }
